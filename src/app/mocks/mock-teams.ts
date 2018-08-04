0f370523140b531024cf77d31cdbd61e967add45
Nick Maxwell <nick.maxwell.gamedev@gmail.com>
2018-08-03 23:18:28 -0400

    things

--- src/app/mocks/mock-teams.ts
+++ src/app/mocks/mock-teams.ts
@@ -1,5 +1,6 @@
 import { Team } from '../models/team';
 
 export const TEAMS: Team[] = [
-  { id: 1, name: 'mock team' , teamMembers: [ 1, 2, 3, 4, 5, 6, 7 ] , rooms: [ 1, 2, 3, 4 ] }
+  { id: 1, name: 'my mock team' , teamMembers: [ 1, 2, 3, 4, 5, 6, 7 ] , rooms: [ 1, 2, 3, 4 ] },
+  { id: 2, name: 'my other mock team' , teamMembers: [ 1, 2, 3, 4, 5, 6, 7 ] , rooms: [ 1, 2, 3, 4 ] }
 ];
